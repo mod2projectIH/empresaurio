11808154007bfa3c7779b03214f1bbec9c77d2bc
Gudurix <gudurix85@gmail.com>
2019-12-12 19:53:42 +0100

    made checkin form v2

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 module.exports.isAuthenticated = (req, res, next) => {
-  if (req.session.user) {
+  if (req.session.worker) {
     next()
   } else {
     req.session.genericError = 'User is not authenticated!'
@@ -8,7 +8,7 @@ module.exports.isAuthenticated = (req, res, next) => {
 }
 
 module.exports.isNotAuthenticated = (req, res, next) => {
-  if (req.session.user) {
+  if (req.session.worker) {
     res.redirect('/');
   } else {
     next()
